4dc068cde37759b2c7997f85d0f7c2d27e170bf3
cjsmith14 <cjs.smith@gmail.com>
2016-02-03 21:52:04 -0500

    Committing initial express app structure 	modified:   app.js 	modified:   package.json 	new file:   public/index.html

--- app.js
+++ app.js
@@ -4,24 +4,26 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+//Middleware
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+//Resources
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/resources', express.static(path.join(__dirname, "node_modules/angular")));
+app.use('/resources', express.static(path.join(__dirname, "node_modules/bootstrap")));
+app.use('/resources', express.static(path.join(__dirname, "node_modules/angular-ui-bootstrap")));
+//Routes
 app.use('/', routes);
 app.use('/users', users);
 
